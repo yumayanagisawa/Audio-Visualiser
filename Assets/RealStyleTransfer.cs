90ff34bb05cfc0f6097f4764d6d1d6b090259c0d
Sky Nite <skygnite@gmail.com>
2019-03-13 00:33:49 -0500

    music in SteamingAssets folder uploaded to scene

---
+++ Assets/RealStyleTransfer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealStyleTransfer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StyleTransferFunction()
+    {
+
+
+        //DeepAI_API api = new DeepAI_API(apiKey: "YOUR_API_KEY");
+
+        //StandardApiResponse resp = api.callStandardApi("neural-style", new
+        //{
+        //    style = File.OpenRead("C:\\path\\to\\your\\file.jpg"),
+        //    content = File.OpenRead("C:\\path\\to\\your\\file.jpg"),
+        //});
+        //Console.Write(api.objectAsJsonString(resp));
+    }
+
+}
---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public List<AudioClip> songs;
+    private int songIndex = 0;
+
+    private string absolutePath = "./music";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetSongsFromFolder();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            musicSource.Stop();
+            musicSource.clip = songs[songIndex];
+            songIndex++;
+            if(songIndex >= songs.Count)
+            {
+                songIndex = 0;
+            }
+            musicSource.Play();
+        }
+        
+    }
+
+    private void GetSongsFromFolder()
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+        FileInfo[] songFiles = directoryInfo.GetFiles("*.*");
+
+        foreach (FileInfo songFile in songFiles)
+        {
+            StartCoroutine(ConvertFilesToAudioClip(songFile));
+            //return; //remove this when doing multiple songs
+        }
+
+    }
+
+    private IEnumerator ConvertFilesToAudioClip(FileInfo songFile)
+    {
+        if (songFile.Name.Contains("meta"))
+            yield break;
+        else
+        {
+            string songName = songFile.FullName.ToString();
+            string url = string.Format("file://{0}", songName);
+            UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.WAV);
+            yield return www.SendWebRequest();
+            var clip = DownloadHandlerAudioClip.GetContent(www);
+            if (clip != null)
+            {
+                songs.Add(clip);
+            }
+        }
+    }
+}
